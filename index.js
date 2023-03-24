a57ed27e23ad411b7f54ec4ca11e03ada623b405
lilasamarji <lilasamarji@gmail.com>
2023-03-24 15:51:19 +0300

    cards

--- index.js
+++ index.js
@@ -7,26 +7,21 @@ fetch('https://restaurants-api-u1o9.onrender.com')
             
                   var cards= document.createElement('div');
                 cards.innerHTML=`
-                 <div class="col-4">
-
-<div class="card" style="width:22.3vw;height:22.3vw;  margin-right: 15vw; margin-bottom:10vw;  box-shadow: rgba(0, 0, 0, 0.35) 0px 2vw 5vw; ">
-     <img src="${restaurant.Picture}" class="card-img-top">
-     <div class="card-body" style="background-color:#CCD1D1;width:22.3vw;height:27.5vw;">
-      <div class="row " style="justify-content:center;>
-       
-       <div  style="color:black ;margin-top:0.3vw;margin-bottom:0.2vw;"><span style="font-weight:bold;color:#B22222;font-size:1.2vw;">Location</span> <p style="font-size:1.5vw; margin-top:0.3vw;margin-bottom:0.2vw;color:black;margin-left:0.2vw" >  ${restaurant.Location}</p> </div>
-       <div class="row" style="justify-content:center;margin-top:0.3vw;">
-       <div style="margin:0.5vw;" >  <a style="color:black" href="${restaurant.Instagram}"><img src="images/instagram.png" style="width:2vw;height:2vw;" /></a></div>
-       <div style="margin:0.5vw; > <a style="color:black" href="tel:${restaurant.Phone}"><img src="images/whatsapp.png" style="width:2vw;height:2vw;" /></a> </div>
-       <div style="margin:0.5vw; >  <a style="color:black" href="tel:${restaurant.facebook}"><img src="images/facebook.png" style="width:2vw;height:2vw;" /></a> </div>
-       </div>
-       </div>
-       </div>
-     </div>
-     </div>
-</div>`;
-cards.setAttribute("class","col-4")
+                <div class="card" style="width:18rem;height:27rem;margin:3rem;">
+                <img class="card-img-top" src="${restaurant.Picture}" alt="Card image cap" style="width:100%;">
+                <div class="card-body" style="background-color:#FEF5E7 ;height:60%;">
+                <div  style="color:black ;margin-top:0.3vw;margin-bottom:3vw;"><span style="font-weight:bold;color:#B22222;font-size:1.2vw;">Location</span> <p style="font-size:1.5vw; margin-top:0.3vw;margin-bottom:0.2vw;color:black;margin-left:0.2vw" >  ${restaurant.Location}</p> </div>
+                <div class="row" style="justify-content:center;margin-top:0.3vw;margin-bottom:3vw;">
+                <div style="margin:0.5vw;" >  <a style="color:black" href="${restaurant.Instagram}"><img src="images/instagram.png" style="width:2vw;height:2vw;" /></a></div>
+                <div style="margin:0.5vw; > <a style="color:black" href="tel:${restaurant.Phone}"><img src="images/whatsapp.png" style="width:2vw;height:2vw;" /></a> </div>
+                <div style="margin:0.5vw; >  <a style="color:black" href="tel:${restaurant.facebook}"><img src="images/facebook.png" style="width:2vw;height:2vw;" /></a> </div>
+                </div>
+                </div>
+              </div>`
+cards.setAttribute("id","cards");
 document.getElementById("contact").appendChild(cards);
+var style=`margin-bottom:20%`;
+document.getElementById("cards").style=style;
 
       
             })
